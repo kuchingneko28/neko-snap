1e361a527417cc65ec45aac213dc3a5a46742fd9
KuchingNeko <kuchingneko28@gmail.com>
2025-07-28 01:37:45 +0800

    idk just test

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,8 @@
 const formatTime = (seconds) => {
-  const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = Math.floor(seconds % 60);
+  const rounded = Math.round(seconds);
+  const h = Math.floor(rounded / 3600);
+  const m = Math.floor((rounded % 3600) / 60);
+  const s = rounded % 60;
   return [h, m, s].map((v) => v.toString().padStart(2, "0")).join(":");
 };
 
